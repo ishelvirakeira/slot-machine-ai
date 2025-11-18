7fbcd4f4a118f1084f1083ebc116f9595c6d2d03
Elvira Keira Ishimwe <ishelvirakeira@gmail.com>
2025-11-18 01:01:10 -0500

    Serve frontend from backend + add app.get('/') route

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const path=require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));//backend serves HTML
+app.use(express.static(path.join(__dirname, "public")));//backend serves HTML
 
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
+//home route
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 // AI Betting Recommendation
 app.post("/ai/bet", async (req, res) => {
   try {
